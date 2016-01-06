bf37f8a9b16af90162cede0c0e5de2d3925506f5
Aleh Atsman <Aleh_Atsman@EPBYGROW0183.grodno.epam.com>
2016-01-06 18:23:49 +0300

    simplewebrtc

--- src/cljs/rtcviewr/core.cljs
+++ src/cljs/rtcviewr/core.cljs
@@ -17,10 +17,11 @@
 
 (toolbarManager (qs ".header") (qs ".controls"))
 
-(def webrtc (js/SimpleWebRTC. (js-obj
-  :localVideoEl "mini-video" 
-  :remoteVideosEl "remotesVideos"
-  :autoRequestMedia true))))
+(def webrtc (js/SimpleWebRTC.
+  #js {
+      :localVideoEl "mini-video"
+      :remoteVideosEl "remotesVideos"
+      :autoRequestMedia true}))
 
 (.on webrtc "readyToCall" (fn[] 
   (.joinRoom webrtc "myroom1")))
