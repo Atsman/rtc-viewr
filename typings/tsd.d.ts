87052d067424be3ea2b5ac8fb164dbc24a936c1d
Aleh Atsman <Aleh_Atsman@EPBYGROW0183.grodno.epam.com>
2016-02-23 11:19:19 +0300

    Merge branch 'starter/master' into origin/angular2
    # Conflicts:
    #	.gitignore
    #	README.md
    #	karma.conf.js
    #	package.json
    #	protractor.conf.js
    #	spec-bundle.js
    #	src/app/app.e2e.ts
    #	src/app/app.ts
    #	src/custom_typings.d.ts
    #	src/index.html
    #	src/main.ts
    #	src/polyfills.ts
    #	tslint.json
    #	typings.json
    #	webpack.config.js
    #	webpack.prod.config.js
    #	webpack.test.config.js

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,3 @@
+/// <reference path="angular2/angular2.d.ts" />
 /// <reference path="lodash/lodash.d.ts" />
 /// <reference path="socket.io-client/socket.io-client.d.ts" />
-/// <reference path="angular2/angular2.d.ts" />
